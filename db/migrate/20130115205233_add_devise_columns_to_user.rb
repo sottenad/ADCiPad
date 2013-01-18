30eddcaf084d9c4e6e2a6e6bd3eef67fed21c2ee
Steve Ottenad <sottenad@gmail.com>
2013-01-17 17:29:36 -0800

    fixed db migrations

--- db/migrate/20130115205233_add_devise_columns_to_user.rb
+++ db/migrate/20130115205233_add_devise_columns_to_user.rb
@@ -2,7 +2,7 @@ class AddDeviseColumnsToUser < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
 	    add_column :users, :token_authenticatable, :string
-	    add_index  :users, :authentication_token, :unique => true
+
     end
   end
   def self.down
