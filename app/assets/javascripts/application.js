f2eea641c9db0f0d612516be7b3a5af13de53a18
Steve Ottenad <sottenad@gmail.com>
2013-05-23 09:05:04 -0700

    asset pipeline!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-//= require jquery_tablesorter_min
-//= require jquery_tablesorter_pager
-//= require jquery_tablesorter_widgets_min
 //= require bootstrap
+//= require jquery_tablesorter_min
 //= require products
+//= require sessions
+
